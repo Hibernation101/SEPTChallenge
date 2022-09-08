72e1f310e744f823a752864f2dc7174e14347a7d
Hibernation101 <s3900509@student.rmit.edu.au>
2022-09-09 00:19:29 +1000

    File Organisation

--- src/main/java/com/example/septchallenge/ItemManagementDAO.java
+++
@@ -1,4 +0,0 @@
-package com.example.septchallenge;
-
-public class ItemManagementDAO {
-}
--- src/main/java/com/sept/philiplee/rest/septchallenge/SeptChallengeApplication.java
+++ src/main/java/com/sept/philiplee/rest/septchallenge/SeptChallengeApplication.java
@@ -1,4 +1,4 @@
-package com.example.septchallenge;
+package com.sept.philiplee.rest.septchallenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
