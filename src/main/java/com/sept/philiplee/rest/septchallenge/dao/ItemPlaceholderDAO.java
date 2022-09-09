bcfabb3890a37b8eae6bebb7c70610c24a5e85ac
Hibernation101 <s3900509@student.rmit.edu.au>
2022-09-09 11:14:20 +1000

    PlaceholderDAO branch initial commit

---
+++ src/main/java/com/sept/philiplee/rest/septchallenge/dao/ItemPlaceholderDAO.java
@@ -0,0 +1,8 @@
+package com.sept.philiplee.rest.septchallenge.dao;
+import com.sept.philiplee.rest.septchallenge.model.Item;
+
+public class ItemPlaceholderDAO extends Item {
+    public void createItem(String id, String name, String desc, double price) {
+
+    }
+}
