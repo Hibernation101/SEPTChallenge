430d3aa30db139f55b5f880b5ef3cf5af3a84b4a
Hibernation101 <s3900509@student.rmit.edu.au>
2022-09-09 11:15:04 +1000

    createItem Implementation

--- src/main/java/com/sept/philiplee/rest/septchallenge/dao/ItemPlaceholderDAO.java
+++ src/main/java/com/sept/philiplee/rest/septchallenge/dao/ItemPlaceholderDAO.java
@@ -3,6 +3,9 @@ import com.sept.philiplee.rest.septchallenge.model.Item;
 
 public class ItemPlaceholderDAO extends Item {
     public void createItem(String id, String name, String desc, double price) {
-
+        setId(id);
+        setName(name);
+        setDesc(desc);
+        setPrice(price);
     }
 }
