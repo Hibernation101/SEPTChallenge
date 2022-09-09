eff35f15cfeaf5f3ad02fc5e98f1f0cf119143df
Hibernation101 <s3900509@student.rmit.edu.au>
2022-09-09 13:31:11 +1000

    GetItem Implementation

--- src/main/java/com/sept/philiplee/rest/septchallenge/model/GetItem.java
+++ src/main/java/com/sept/philiplee/rest/septchallenge/model/GetItem.java
@@ -1,4 +1,22 @@
 package com.sept.philiplee.rest.septchallenge.model;
 
+import java.util.ArrayList;
+
 public class GetItem {
+    public String getItemID(Item item){
+        String foundID = item.getId();
+        return foundID;
+    }
+    public String getItemName(Item item){
+        String foundName = item.getName();
+        return foundName;
+    }
+    public String getItemDesc(Item item){
+        String foundDesc = item.getDesc();
+        return foundDesc;
+    }
+    public double getItemPrice(Item item){
+        double foundPrice = item.getPrice();
+        return foundPrice;
+    }
 }
